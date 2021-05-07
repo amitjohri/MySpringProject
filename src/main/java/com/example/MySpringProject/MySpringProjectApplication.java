5aa082381283de6b3ac2c75d05e64779078fe349
amitjohri <55690089+amitjohri@users.noreply.github.com>
2021-05-08 00:18:59 +0530

    sysout change

--- src/main/java/com/example/MySpringProject/MySpringProjectApplication.java
+++ src/main/java/com/example/MySpringProject/MySpringProjectApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MySpringProjectApplication {
 
 	public static void main(String[] args) {
-		System.out.println("HELLO INDIA :)");
+		System.out.println("HELLO INDIA again :)");
 		SpringApplication.run(MySpringProjectApplication.class, args);
 	}
 
