54440ccc4718bd1ea04a167a036c3971fc809ab3
Amit <amit.johri@atcs.com>
2021-05-07 21:27:59 +0530

    first commit

---
+++ src/main/java/com/example/MySpringProject/MySpringProjectApplication.java
@@ -0,0 +1,14 @@
+package com.example.MySpringProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringProjectApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+		SpringApplication.run(MySpringProjectApplication.class, args);
+	}
+
+}
